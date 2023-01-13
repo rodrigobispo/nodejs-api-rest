78d77c20ab0b2a919d8e4e58bcf7d49adc35bd8a
rodrigobispo <rodrigoaugustocb@gmail.com>
2023-01-13 20:05:27 -0300

    Implementação do post, adicionando em constante json

--- src/app.js
+++ src/app.js
@@ -1,18 +1,26 @@
 import express from "express";
 
-const app =  express();
+const app = express();
+
+app.use(express.json());
 
 const livros = [
-    {id: 1, "titulo": "Senhor dos Aneis"},
-    {id: 2, "titulo": "Harry Potter"}
+    { id: 1, "titulo": "Senhor dos Aneis" },
+    { id: 2, "titulo": "Harry Potter" }
 ]
 
-app.get('/', (request, response) => {
-    response.status(200).send('Estudo de Node');
+app.get('/', (req, res) => {
+    res.status(200).send('Estudo de Node');
 })
 
-app.get('/livros', (request, response) => {
-    response.status(200).json(livros);
+app.get('/livros', (req, res) => {
+    res.status(200).json(livros);
+})
+
+app.post('/livros', (req, res) => {
+    livros.push(req.body)
+    res.status(201).send('Livro foi cadastrado com sucesso.');
+
 })
 
 export default app
