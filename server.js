ee7c6207f87ce44d831ff3dee487fa9130557f23
rodrigobispo <rodrigoaugustocb@gmail.com>
2023-01-13 14:49:54 -0300

    Criação de um servidor http que retorna texto

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require("http");
+const port = 3000;
+
+const server = http.createServer((request, response) => {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.end('Curso de Node');
+})
+server.listen(port, () => {
+    console.log(`Servidor escutando em http://localhost:${port}`);
+})
