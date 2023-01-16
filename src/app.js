fb1018f950bf729fc6dae3962742acfd6e71db22
rodrigobispo <rodrigoaugustocb@gmail.com>
2023-01-16 11:00:29 -0300

    Métodos put (atualização de título livro) e get pelo id

--- src/app.js
+++ src/app.js
@@ -17,10 +17,24 @@ app.get('/livros', (req, res) => {
     res.status(200).json(livros);
 })
 
+app.get('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.params.id);
+    return res.json(livros[index]);
+})
+
 app.post('/livros', (req, res) => {
     livros.push(req.body)
     res.status(201).send('Livro foi cadastrado com sucesso.');
-
 })
+
+app.put('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    return res.json(livros);
+})
+
+function buscaLivro(id) {
+    return livros.findIndex(livro => livro.id == id)
+}
 
 export default app
