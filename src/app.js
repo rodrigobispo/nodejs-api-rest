5a1ec7151a727d171fa0979f1e87d96dc3bd8f66
rodrigobispo <rodrigoaugustocb@gmail.com>
2023-01-16 11:13:27 -0300

    Método delete

--- src/app.js
+++ src/app.js
@@ -33,6 +33,13 @@ app.put('/livros/:id', (req, res) => {
     return res.json(livros);
 })
 
+app.delete('/livros/:id', (req, res) => {
+    let {id} = req.params;
+    let index = buscaLivro(id);
+    livros.splice(index, 1);
+    return res.send(`Livro ${id} removido com sucesso.`);
+})
+
 function buscaLivro(id) {
     return livros.findIndex(livro => livro.id == id)
 }
